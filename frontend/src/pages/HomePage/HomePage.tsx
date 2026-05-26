c804f4a4ee125ff01c0c43d8697a416c810eeb3b
Nina <nina.stevoska@student.um.si>
2026-05-26 08:33:06 +0200

    Upam koncno odpravljen scrollbar

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -60,7 +60,7 @@ function HomePage() {
 	}, [isSearchActive])
 
 	return (
-		<main className="home-scroll relative isolate min-h-screen overflow-x-hidden bg-[#fffdf8] text-[#111111] lg:h-screen lg:snap-y lg:snap-mandatory lg:overflow-y-auto lg:scroll-smooth">
+		<main className="relative isolate min-h-screen overflow-x-hidden bg-[#fffdf8] text-[#111111] [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden lg:h-screen lg:snap-y lg:snap-mandatory lg:overflow-y-auto lg:scroll-smooth">
 			<HomeBackground />
 
 			<div
